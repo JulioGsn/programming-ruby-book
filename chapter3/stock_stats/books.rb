7773b4260aff2c4f1c81c0b861eceda7c1fc1a4f
JulioGsn <juliogsn15@outlook.com>
2025-01-18 12:46:35 -0300

    Finish first example of classes

--- chapter3/stock_stats/books.rb
+++ chapter3/stock_stats/books.rb
@@ -6,34 +6,11 @@ class BookInStock
     @price = Float(price)
   end
 
+  def price_in_cents
+    (price * 100).round
+  end 
+
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
 end
-
-class CsvReader
-  def initialize
-    @books_in_stock = []
-  end
-
-  def read_in_csv_data(csv_file_name)
-    CSV.foreach(csv_file_name, headers: true) do |row|
-      @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])
-    end
-  end
-  
-  def total_value_in_stock
-    # later we'll see easier ways to sum a collection
-    sum = 0.0
-    @books_in_stock.each { |book| sum += book.price_in_cents }
-    sum / 100.0
-  end
-
-  def number_of_each_isbn
-  end
-end
-
-reader = CsvReader.new
-reader.read_in_csv_data "stock.csv"
-
-puts "Total value in stock = #{reader.total_value_in_stock}"
